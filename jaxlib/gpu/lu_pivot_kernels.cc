fc1dcdc3a32c3d54e40b4365fd2684fb11364926
Dan Foreman-Mackey <danfm@google.com>
2024-06-20 08:28:08 -0700

    Update lu_pivots_to_permutation to use FFI dimensions on GPU.
    The XLA FFI interface provides metadata about buffer dimensions, so quantities
    like batch dimensions can be evaluated on the backend, instead of passed as
    attributes. This change has the added benefit of allowing this FFI call to
    support "vectorized" vmap and dynamic shapes.
    
    PiperOrigin-RevId: 645041317

--- jaxlib/gpu/lu_pivot_kernels.cc
+++ jaxlib/gpu/lu_pivot_kernels.cc
@@ -16,8 +16,13 @@ limitations under the License.
 #include "jaxlib/gpu/lu_pivot_kernels.h"
 
 #include <cstdint>
+#include <functional>
+#include <limits>
+#include <stdexcept>
 #include <string>
 
+#include "absl/algorithm/container.h"
+#include "absl/strings/str_format.h"
 #include "jaxlib/gpu/gpu_kernel_helpers.h"
 #include "jaxlib/gpu/vendor.h"
 #include "xla/ffi/api/c_api.h"
@@ -28,29 +33,41 @@ namespace JAX_GPU_NAMESPACE {
 
 namespace ffi = xla::ffi;
 
-XLA_FFI_Error* LuPivotsToPermutation(XLA_FFI_CallFrame* call_frame) {
-  static const auto* kImpl =
-      ffi::Ffi::Bind()
-          .Ctx<ffi::PlatformStream<gpuStream_t>>()
-          .Attr<std::int64_t>("batch_size")
-          .Attr<std::int32_t>("pivot_size")
-          .Attr<std::int32_t>("permutation_size")
-          .Arg<ffi::Buffer<ffi::DataType::S32>>()
-          .Ret<ffi::Buffer<ffi::DataType::S32>>()
-          .To([](gpuStream_t stream, std::int64_t batch_size,
-                 std::int32_t pivot_size, std::int32_t permutation_size,
-                 auto pivots, auto permutation) -> ffi::Error {
-            LaunchLuPivotsToPermutationKernel(stream, batch_size, pivot_size,
-                                              permutation_size, pivots.data,
-                                              permutation->data);
-            if (auto status = JAX_AS_STATUS(gpuGetLastError()); !status.ok()) {
-              return ffi::Error(static_cast<XLA_FFI_Error_Code>(status.code()),
-                                std::string(status.message()));
-            }
-            return ffi::Error::Success();
-          })
-          .release();
-  return kImpl->Call(call_frame);
+template <typename T>
+inline T CastNoOverflow(std::int64_t value,
+                        const std::string& source = __FILE__) {
+  if constexpr (sizeof(T) == sizeof(std::int64_t)) {
+    return value;
+  } else {
+    if (value > std::numeric_limits<T>::max()) [[unlikely]] {
+      throw std::overflow_error{
+          absl::StrFormat("%s: Value (=%d) exceeds the maximum representable "
+                          "value of the desired type",
+                          source, value)};
+    }
+    return static_cast<T>(value);
+  }
+}
+
+ffi::Error LuPivotsToPermutationImpl(
+    gpuStream_t stream, std::int32_t permutation_size,
+    ffi::Buffer<ffi::DataType::S32> pivots,
+    ffi::Result<ffi::Buffer<ffi::DataType::S32>> permutation) {
+  auto dims = pivots.dimensions;
+  std::int32_t pivot_size = CastNoOverflow<std::int32_t>(dims.back());
+  std::int64_t batch_size = 1;
+  if (dims.size() >= 2) {
+    batch_size =
+        absl::c_accumulate(dims.first(dims.size() - 1), 1, std::multiplies<>());
+  }
+  LaunchLuPivotsToPermutationKernel(stream, batch_size, pivot_size,
+                                    permutation_size, pivots.data,
+                                    permutation->data);
+  if (auto status = JAX_AS_STATUS(gpuGetLastError()); !status.ok()) {
+    return ffi::Error(static_cast<XLA_FFI_Error_Code>(status.code()),
+                      std::string(status.message()));
+  }
+  return ffi::Error::Success();
 }
 
 }  // namespace JAX_GPU_NAMESPACE
